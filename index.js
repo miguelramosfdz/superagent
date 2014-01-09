ae2c4a30c943b8706fff57f6156fa6dfb2eed89a
Christian Sullivan <cs@euforic.co>
2014-01-09 15:37:39 -0800

    add attach function for titanium mutlipart requests

--- index.js
+++ index.js
@@ -5,4 +5,24 @@ require('tipm-xhrpoly');
 
 //expose superagent
 
-module.exports = require('superagent');
+var Agent = module.exports = require('visionmedia-superagent');
+
+/**
+ * attach file to Request
+ * @param  {String} name
+ * @param  {String} path
+ * @param  {String} filename
+ */
+
+Agent.Request.prototype.attach = function(name, path, filename) {
+  if (filename) { console.warn('Setting upload\'s filename is currently not supported'); }
+
+  var file = Ti.Filesystem.getFile(path);
+  var attachment = {};
+  attachment[name] = file.read();
+
+  this.type('multipart/form-data');
+  this.send(attachment);
+
+  return this;
+};
